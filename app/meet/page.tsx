51d2431f618ca882361d30578d2f979c2bb205f9
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-10-24 23:49:27 +0300

    meet route implementation

---
+++ app/meet/page.tsx
@@ -0,0 +1,7 @@
+export default function Meet() {
+  return (
+    <main className="flex h-screen items-center justify-center md:px-24 bg-gradient-to-br from-indigo-950 to-indigo-600">
+      <iframe src='https://outlook.office365.com/owa/calendar/CustomerSuccess@kali.so/bookings/' width='100%' height='100%'></iframe>
+    </main>
+  );
+}
