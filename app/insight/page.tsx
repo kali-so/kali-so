4eb5b3a8104917122b5127593de3723993799363
Batuhan Ergun <ergunbatuhan@icloud.com>
2023-11-19 15:44:05 +0100

    iframe fix

--- app/insight/page.tsx
+++ app/insight/page.tsx
@@ -1,7 +1,7 @@
 export default function Meet() {
   return (
-    <main className="flex h-screen items-center justify-center md:px-4 md:py-4 bg-gradient-to-br from-indigo-950 to-indigo-600">
-      <iframe width="100%" height="100%" frameBorder="0" allowFullScreen src="https://app.posthog.com/embedded/h9aIBV4BnnV0V5-YAqHI_y_XRlYAxg?whitelabel"></iframe>
+    <main className="flex h-screen items-center justify-center md:px-4 md:py-4 bg-white">
+      <iframe width="100%" height="100%" src="https://app.posthog.com/embedded/h9aIBV4BnnV0V5-YAqHI_y_XRlYAxg?whitelabel"></iframe>
     </main>
   );
 }
